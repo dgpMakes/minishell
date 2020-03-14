b362b6f8d74338b90e964a093953efa5121a4b74
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2020-03-14 18:56:52 +0100

    Update msh.c

--- msh.c
+++ msh.c
@@ -6,7 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <wait.h>
+#include <wait.h> gives an error (?)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
