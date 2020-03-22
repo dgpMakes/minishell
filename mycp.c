60055ed19baa4715046e9ee84cca3672e467e2a7
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2020-03-22 10:47:14 +0100

    updated FULLY WORKING!

--- mycp.c
+++ mycp.c
@@ -15,22 +15,41 @@
 
 int main(int argc, char *argv[])  // [1] original archive, [2] to paste in file (destination)
 {
-	int source_descriptor, destination_descriptor;		  /*to identify a file that already exists*/
+	int source_descriptor;		  /*to identify a file that already exists*/
 	DIR *dir;                 /*the destination path*/
 
 	FILE *source_file, *destination_file;
     struct dirent *destination_read;    /*to read the destination path*/
     char count;  /*to read entire path*/
+	struct stat prove_structure; /*to prove the structure of the given arguments*/
 
 	/*checks the command is properly written*/
-	if (argc < 2)
+	if (argc != 3)
 	{
 		fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
 		return -1;
 	}
 	
+	if(stat(argv[1],&prove_structure) == 0)
+	{
+    	if(prove_structure.st_mode & S_IFDIR)
+    	{
+        	fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
+			return -1;
+   		}
+	}
+	if( stat(argv[2],&prove_structure) == 0)
+	{
+    	if( prove_structure.st_mode & S_IFDIR)
+    	{
+        	fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
+			return -1;
+   		}
+	}
+
 	/*opens file and addresses it a descriptor*/
 	source_descriptor = open(argv[1], O_RDONLY);
+
 	/*if f_descriptor is -1, the file cannot be opened*/
 	if (source_descriptor == -1)
 	{ 
@@ -38,20 +57,19 @@ int main(int argc, char *argv[])  // [1] original archive, [2] to paste in file 
 		return -1;
 	}
 
-	/*we check the destination using its descriptor*/
-	destination_descriptor = open(argv[2], O_RDONLY);
-	/*if f_descriptor is -1, the file cannot be opened*/
-	if (destination_descriptor == -1)
-	{ 
-		fprintf(stderr, "[ERROR] Error opening the copied file");
+	/*opens the requested file*/
+	source_file = fopen(argv[1], "r"); //we open in mode reading and check it is correct
+	if(source_file == NULL){
+		fprintf(stderr, "[ERROR] Error opening original file");
 		return -1;
 	}
 
-	/*opens the requested file*/
-	source_file = fopen(argv[1], "r"); //we open in mode reading and check it is correct
-
-	/*we create the new file and check it is correct*/
-	destination_file= fopen(argv[2], "w");
+	/*creates the new file*/
+	destination_file= fopen(argv[2], "w"); //we create the new file and check it is correct
+	if(destination_file == NULL){
+		fprintf(stderr, "[ERROR] Error opening the copied file");
+		return -1;
+	}
 
 	while( ( count = fgetc(source_file) ) != EOF )
       fputc(count, destination_file);
