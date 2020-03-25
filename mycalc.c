a760a1389ff2749c2deb9392e2568b346a47475b
ladydandelion <evahuertasmaldonado@gmail.com>
2020-03-25 19:38:31 +0100

    silly

--- mycalc.c
+++ mycalc.c
@@ -6,20 +6,23 @@
 #include <sys/types.h> /* Header file for system calls opendir, readdir and closedir */
 #include <dirent.h>
 #include <limits.h> /* For PATH_MAX in MacOS */
+#include <stdlib.h>
+
 
 /* Error handling libraries */
 #include <errno.h>
 #include <string.h> /* Used for strerror(errno) and strcmp()*/
 
-extern char** environ; //Declaring the environment variable
+//extern char** environ; //Declaring the environment variable
 int main(int argc, char *argv[]) //[1] counts the number of strings introduce [2] store them in an array 
 {
 int operand1, operand2, result1, result2, resultop1, resultop2;
+int Acc=0;
+char environmentVariable [3]="Acc";
+char value[100];
 
-char** Acc;
-Acc=environ;
-char operator1 []="add";
-char operator2 []="mod";
+char operator1 [3]="add";
+char operator2 [3]="mod";
 
  /*Checks the comand is properly written */
    if(argc != 3)
@@ -29,28 +32,30 @@ char operator2 []="mod";
    }
 
 /*Check if the operands are integers*/
-if(sizeof(argv[1]==2) && sizeof(argv[3]==2))
+if(sizeof(argv[1])==2 && sizeof(argv[3])==2)
 {
-    operand1 = argv[0];
-    operand2 = argv[2];
+    operand1 =(int) argv[1];
+    operand2 =(int) argv[3];
 
 }else{
-    fprint(stderr, "[ERROR] The structure of the command is <operand1><add/mod><operand2>\n");
+    fprintf(stderr, "[ERROR] The structure of the command is <operand1><add/mod><operand2>\n");
     return -1;
 }
 
 /*Checks if the operator is correct*/
-result1 = strcmp(argv[2], operator1);
-result2 = strcmp(argv[2], operator2);
+result1 = strcmp(argv[1], operator1);
+result2 = strcmp(argv[1], operator2);
 
 if (result1!=0 || result2!=0)
 {
-   fprint(stderr, "[ERROR] The structure of the command is <operand1><add/mod><operand2>\n"); 
+   fprintf(stderr, "[ERROR] The structure of the command is <operand1><add/mod><operand2>\n"); 
    return -1;
 }else{
     if (result1 == 0){
         Acc= operand1 + operand2;
-        printf("Acc =%d",Acc);
+        sprintf(value,"%d",Acc);
+        setenv(environmentVariable, value ,1);
+        
 
     }else{
         resultop1= operand1 % operand2;
