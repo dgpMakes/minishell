75d81df40e9cf98c265ba946d2b2f2c691341f89
dgpMakes <dgpmakes@gmail.com>
2020-04-02 01:27:28 +0200

    mycp passed

--- msh.c
+++ msh.c
@@ -51,6 +51,71 @@ void getCompleteCommand(char*** argvv, int num_command) {
         argv_execvp[i] = argvv[num_command][i];
 }
 
+int mycp(FILE *source_file, FILE *destination_file)  // [1] original archive, [2] to paste in file (destination)
+{
+	int source_descriptor;		  /*to identify a file that already exists*/
+    struct dirent *destination_read;    /*to read the destination path*/
+    char count;  /*to read entire path*/
+	struct stat prove_structure; /*to prove the structure of the given arguments*/
+
+	/*checks the command is properly written*/
+	if (argc != 3)
+	{
+		fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
+		return -1;
+	}
+	
+	if(stat(argv[1],&prove_structure) == 0)
+	{
+    	if(prove_structure.st_mode & S_IFDIR)
+    	{
+        	fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
+			return -1;
+   		}
+	}
+	if( stat(argv[2],&prove_structure) == 0)
+	{
+    	if( prove_structure.st_mode & S_IFDIR)
+    	{
+        	fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
+			return -1;
+   		}
+	}
+
+	/*opens file and addresses it a descriptor*/
+	source_descriptor = open(argv[1], O_RDONLY);
+
+	/*if f_descriptor is -1, the file cannot be opened*/
+	if (source_descriptor == -1)
+	{ 
+		fprintf(stderr, "[ERROR] Error opening original file");
+		return -1;
+	}
+
+	/*opens the requested file*/
+	source_file = fopen(argv[1], "r"); //we open in mode reading and check it is correct
+	if(source_file == NULL){
+		fprintf(stderr, "[ERROR] Error opening original file");
+		return -1;
+	}
+
+	/*creates the new file*/
+	destination_file= fopen(argv[2], "w"); //we create the new file and check it is correct
+	if(destination_file == NULL){
+		fprintf(stderr, "[ERROR] Error opening the copied file");
+		return -1;
+	}
+
+	while( ( count = fgetc(source_file) ) != EOF )
+      fputc(count, destination_file);
+ 
+   printf("File copied successfully.\n");
+ 
+   fclose(source_file);
+   fclose(destination_file);
+	return 0;
+}
+
 void mycalc(int operand1, int operand2, int *Acc, char *operator){
     int result;
     if (strcmp(operator,"mod")==0){
