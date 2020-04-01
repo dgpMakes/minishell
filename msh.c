2662ba2d5536f52bd6dfb3c019f59e9122292808
ladydandelion <evahuertasmaldonado@gmail.com>
2020-04-01 16:21:30 +0200

    introduce mycalc

--- msh.c
+++ msh.c
@@ -3,16 +3,20 @@
 
 #include <parser.h>
 #include <stddef.h>			/* NULL */
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <wait.h> //DIEGO: this gives an error (?)
+#include <sys/types.h>/* Header file for system call open */
+#include <sys/stat.h> /* Defines the structure of data returned by functions fsat(), lstat() and stat() */
+#include <fcntl.h> /* Manipulate file descriptor */
+#include <wait.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <unistd.h> /* Header file for system calls read, write y close */
 #include <sys/wait.h>
 #include <signal.h>
+
+/* Error handling libraries */
+#include <errno.h>
+#include <string.h> /* Used for strerror(errno) and strcmp()*/
 
 #define MAX_COMMANDS 8
 
@@ -47,6 +51,22 @@ void getCompleteCommand(char*** argvv, int num_command) {
         argv_execvp[i] = argvv[num_command][i];
 }
 
+void mycalc(int operand1, int operand2, int *Acc, char *operator){
+    int result;
+    if (strcmp(operator,"mod")==0){
+        result=operand1%operand2;
+        int aux=operand1/operand2;
+        printf("[OK] %d mod %d = %d * %d + %d\n",operand1,operand2,operand2,aux,result);
+    } else if(strcmp(operator,"add")==0){
+        result= operand1 + operand2;
+        *Acc += result;
+        printf("[OK] %d + %d = %d; Acc %d\n",operand1,operand2,result,*Acc);
+    }else{
+        fprintf(stderr, "[ERROR] Error opening the copied file");
+		
+    }
+
+}
 
 /**
  * Main sheell  Loop  
