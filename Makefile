8ba46861bd8695707c10c4ecf66263d414963aa5
Raúl P <raporpe@gmail.com>
2020-04-03 03:11:54 +0200

    Refactoring and corrections

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
-current_dir := $(shell pwd)
-export LD_LIBRARY_PATH := $current_dir:$LD_LIBRARY_PATH
 CC=gcc
 FLAGS=-Wno-implicit-function-declaration
 CFLAGS=-I.
--- msh.c
+++ msh.c
@@ -1,6 +1,3 @@
-//  MSH main file
-// Write your msh source code here
-
 #include <parser.h>
 #include <stddef.h>	
 #include <sys/types.h>/* Header file for system call open */
@@ -20,46 +17,164 @@
 
 #define MAX_COMMANDS 8
 
+//Singal handlers
+void sigchild_handler(int param);
+void siginthandler(int param);
 
-// ficheros por si hay redirección
+//Built-in shell functions
+int mycp(char *source_string, char *destination_string);
+void mycalc(int operand1, char *operator, int operand2, int *Acc);
+
+//Self made functions
+int ioRedirect(char filev[3][64]);
+int createSimpleProcess(char ***argvv, char filev[3][64], int in_background);
+int nicePipe(char ***argvv, char filev[3][64], int in_background);
+
+//Helper function
+int count_elements(char **argvv);
+
+// Redirection files
 char filev[3][64];
 
 //to store the execvp second parameter
 char *argv_execvp[8];
-pid_t child_pid_bg = -1;
 
-
-
-void siginthandler(int param)
-{
-	printf("****  Saliendo del MSH **** \n");
-	//signal(SIGINT, siginthandler);
-        exit(0);
-}
-
-void sigchild_handler(int param)
-{
-    printf("Process in background with pid %i just finished \n", (int)child_pid_bg);
-}
 
 /**
- * Get the command with its parameters for execvp
- * Execute this instruction before run an execvp to obtain the complete command
- * @param argvv
- * @param num_command
- * @return
+ * Main sheell  Loop  
  */
-void getCompleteCommand(char*** argvv, int num_command) {
-    //reset first
-    for(int j = 0; j < 8; j++)
-        argv_execvp[j] = NULL;
+int main(int argc, char* argv[])
+{
+    /**** Do not delete this code.****/
+    int end = 0; 
+    int executed_cmd_lines = -1;
+    char *cmd_line = NULL;
+    char *cmd_lines[10];
 
-    int i = 0;
-    for ( i = 0; argvv[num_command][i] != NULL; i++)
-        argv_execvp[i] = argvv[num_command][i];
+    if (!isatty(STDIN_FILENO)) {
+        cmd_line = (char*)malloc(100);
+        while (scanf(" %[^\n]", cmd_line) != EOF){
+            if(strlen(cmd_line) <= 0) return 0;
+            cmd_lines[end] = (char*)malloc(strlen(cmd_line)+1);
+            strcpy(cmd_lines[end], cmd_line);
+            end++;
+            fflush (stdin);
+            fflush(stdout);
+        }
+    }
+
+    /*********************************/
+
+    int num_commands;
+    char ***argvv = NULL; 
+
+    //Main while
+	while (1) 
+	{
+		int status = 0;
+	    int command_counter = 0;
+        int in_background = 0;
+
+
+		signal(SIGINT, siginthandler);
+        signal(SIGCHLD, sigchild_handler);
+
+
+		// Prompt 
+		write(STDERR_FILENO, "MSH>>", strlen("MSH>>"));
+
+		// Get command
+        //********** DO NOT MODIFY THIS PART. IT DISTINGUISH BETWEEN NORMAL/CORRECTION MODE***************
+        executed_cmd_lines++;
+        if( end != 0 && executed_cmd_lines < end) {
+            command_counter = read_command_correction(&argvv, filev, &in_background, cmd_lines[executed_cmd_lines]);
+        }else if( end != 0 && executed_cmd_lines == end){
+            return 0;
+        }else{
+            command_counter = read_command(&argvv, filev, &in_background); //NORMAL MODE
+        }
+        //************************************************************************************************
+
+
+        /************************ STUDENTS CODE ********************************/
+        if(command_counter <= 0){
+            perror("Error");
+            continue;
+        } 
+        if (command_counter > MAX_COMMANDS){
+            printf("Error: Numero máximo de comandos es %d \n", MAX_COMMANDS);
+            continue;
+        }
+       
+        // Mycalc function
+        if(strcmp(**argvv, "mycalc") == 0 && command_counter == 1) {
+            //Call mycalc
+            mycalc(atoi(argvv[0][1]), argvv[0][2], atoi(argvv[0][3]), 0);
+            continue;
+        }
+
+        //Mycp function
+        if (strcmp(*argvv[0], "mycp") == 0 && command_counter == 1) {//[[mycp],[hola],[adios], NULL]]
+            //Call mycp
+
+            /*checks the command is properly written*/
+            if (count_elements(*argvv) != 3) {
+                fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
+                continue;
+            }
+
+            mycp(argvv[0][1], argvv[0][2]);
+        }
+
+
+        if(command_counter == 1){
+            createSimpleProcess(argvv, filev, in_background);
+            continue;
+        }
+
+        
+        if(command_counter > 1){
+            nicePipe(argvv, filev, in_background);
+        }
+        
+    }
+
+	return 0;
+
 }
 
-int ioRedirect(char filev[3][64]){//
+
+
+
+
+int nicePipe(char ***argvv, char filev[3][64], int in_background){
+    return 1;
+}
+
+
+int createSimpleProcess(char ***argvv, char filev[3][64], int in_background){
+
+    //Create a pipe for the child
+    int pid;
+    switch(pid = fork()){
+        case 0:
+            //The child
+            ioRedirect(filev);
+            execvp(**argvv, *argvv);
+            break;
+        case -1:
+            //Error
+            break;
+        default:
+            //Parent
+            if(in_background) printf("[%i]\n",pid);
+            if(!in_background) wait(&pid);
+    }
+
+}
+
+
+int ioRedirect(char filev[3][64]){
 
     /*For stdin*/
     if(strcmp(filev[0], "0") != 0){
@@ -77,7 +192,7 @@ int ioRedirect(char filev[3][64]){//
 
     /*For stdout*/
     if(strcmp(filev[1], "0") != 0){
-        int fd = open(filev[1], O_RDWR|O_APPEND|O_CREAT, 0600);
+        int fd = open(filev[1], O_RDWR|O_TRUNC|O_CREAT, 0600);
         close(STDOUT_FILENO);
 
         /*Safely open*/
@@ -89,7 +204,7 @@ int ioRedirect(char filev[3][64]){//
     
     /*For stderr*/
     if(strcmp(filev[2], "0") != 0){
-        int fd = open(filev[2], O_RDWR|O_APPEND|O_CREAT, 0600);
+        int fd = open(filev[2], O_RDWR|O_TRUNC|O_CREAT, 0600);
         close(STDERR_FILENO);
         /*Safely open*/
         int new_fd = dup(fd);
@@ -99,15 +214,40 @@ int ioRedirect(char filev[3][64]){//
 
 }
 
+
+
+
+
+//Self implemented functions
+
+void mycalc(int operand1, char *operator, int operand2, int *Acc){
+
+    int result;
+    if (strcmp(operator, "mod") == 0){
+        result = operand1 % operand2;
+        int aux = operand1/operand2;
+        printf("[OK] %d mod %d = %d * %d + %d\n",operand1,operand2,operand2,aux,result);
+    } else if(strcmp(operator,"add")==0){
+        result = operand1 + operand2;
+        *Acc += result;
+        printf("[OK] %d + %d = %d; Acc %d\n", operand1, operand2, result, *Acc);
+    }else{
+        fprintf(stderr, "[ERROR] Error opening the copied file");
+		
+    }
+
+}
+
+
+
 int mycp(char *source_string, char *destination_string)  // [1] original archive, [2] to paste in file (destination)
 {
-    write(STDOUT_FILENO, "1.1", strlen("111"));
     char count;  /*to read entire path*/
     struct stat prove_structure; /*to prove the structure of the given arguments*/
 	int source_descriptor;/*to identify a file that already exists*/
 
     if(stat(source_string, &prove_structure) == 0)
-        {
+    {
         if(prove_structure.st_mode & S_IFDIR)
         {
             fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
@@ -116,7 +256,7 @@ int mycp(char *source_string, char *destination_string)  // [1] original archive
     }
 
    if(stat(destination_string, &prove_structure) == 0)
-    {
+   {
         if(prove_structure.st_mode & S_IFDIR)
         {
             fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
@@ -148,149 +288,56 @@ int mycp(char *source_string, char *destination_string)  // [1] original archive
     }
 
 	while( ( count = fgetc(source_file) ) != EOF )
-      fputc(count, destination_file);
+    fputc(count, destination_file);
  
-   printf("File copied successfully.\n");
+    printf("File copied successfully.\n");
  
-   fclose(source_file);
-   fclose(destination_file);
+    fclose(source_file);
+    fclose(destination_file);
 	return 0;
 }
 
-void mycalc(int operand1, char *operator, int operand2, int *Acc){
 
-    int result;
-    if (strcmp(operator, "mod") == 0){
-        result = operand1 % operand2;
-        int aux = operand1/operand2;
-        printf("[OK] %d mod %d = %d * %d + %d\n",operand1,operand2,operand2,aux,result);
-    } else if(strcmp(operator,"add")==0){
-        result = operand1 + operand2;
-        *Acc += result;
-        printf("[OK] %d + %d = %d; Acc %d\n", operand1, operand2, result, *Acc);
-    }else{
-        fprintf(stderr, "[ERROR] Error opening the copied file");
-		
-    }
-
-}
-
+//Helper function
 int count_elements(char **argvv){//[asdf],[asdf]
     int i = 0;
     for (;argvv[i] != NULL; i++){
-        printf("%s\n",argvv[i]);
     }
-    printf("%i\n", i);
     return i;
 }
 
-/**
- * Main sheell  Loop  
- */
-int main(int argc, char* argv[])
+
+//Signal handler functions
+void siginthandler(int param)
 {
-    /**** Do not delete this code.****/
-    int end = 0; 
-    int executed_cmd_lines = -1;
-    char *cmd_line = NULL;
-    char *cmd_lines[10];
+	printf("****  Saliendo del MSH **** \n");
+	//signal(SIGINT, siginthandler);
+    exit(0);
+}
 
-    if (!isatty(STDIN_FILENO)) {
-        cmd_line = (char*)malloc(100);
-        while (scanf(" %[^\n]", cmd_line) != EOF){
-            if(strlen(cmd_line) <= 0) return 0;
-            cmd_lines[end] = (char*)malloc(strlen(cmd_line)+1);
-            strcpy(cmd_lines[end], cmd_line);
-            end++;
-            fflush (stdin);
-            fflush(stdout);
-        }
-    }
-
-    /*********************************/
-
-    char ***argvv = NULL; 
-    int num_commands;
+void sigchild_handler(int param)
+{
+    //Wait for any child
+    int pid = wait(NULL);
+    //printf("Process in background with pid %i just finished \n", pid);
+}
 
 
-	while (1) 
-	{
-		int status = 0;
-	    int command_counter = 0;
-		int in_background = 0;
-		signal(SIGINT, siginthandler);
-        signal(SIGCHLD, sigchild_handler);
+//Shitty Unused function
 
+/**
+ * Get the command with its parameters for execvp
+ * Execute this instruction before run an execvp to obtain the complete command
+ * @param argvv
+ * @param num_command
+ * @return
+ */
+void getCompleteCommand(char*** argvv, int num_command) {
+    //reset first
+    for(int j = 0; j < 8; j++)
+        argv_execvp[j] = NULL;
 
-		// Prompt 
-		write(STDERR_FILENO, "MSH>>", strlen("MSH>>"));
-
-		// Get command
-                //********** DO NOT MODIFY THIS PART. IT DISTINGUISH BETWEEN NORMAL/CORRECTION MODE***************
-                executed_cmd_lines++;
-                if( end != 0 && executed_cmd_lines < end) {
-                    command_counter = read_command_correction(&argvv, filev, &in_background, cmd_lines[executed_cmd_lines]);
-                }else if( end != 0 && executed_cmd_lines == end){
-                    return 0;
-                }else{
-                    command_counter = read_command(&argvv, filev, &in_background); //NORMAL MODE
-                }
-		            
-
-                //************************************************************************************************
-
-
-              /************************ STUDENTS CODE ********************************/
-	            if (command_counter > 0) {
-                    if (command_counter > MAX_COMMANDS)
-                        printf("Error: Numero máximo de comandos es %d \n", MAX_COMMANDS);
-                    else {
-                        // Print command
-                        
-
-                        if(command_counter == 1){//[[mycalc],[3],[+],[4]]
-
-                            if(strcmp(**argvv, "mycalc") == 0){
-                                //Call mycalc
-                                mycalc(atoi(argvv[0][1]), argvv[0][2], atoi(argvv[0][3]), 0);
-
-                            } else if (strcmp(*argvv[0], "mycp") == 0){//[[mycp],[hola],[adios], NULL]]
-                                //Call mycp
-
-
-                                /*checks the command is properly written*/
-                                if (count_elements(*argvv) != 3) //RAUL
-                                {
-                                    fprintf(stderr,  "[ERROR] The structure of the command is mycp <original file> <copied file>\n");
-                                    continue;
-                                }
-
-                                mycp(argvv[0][1], argvv[0][2]);
-
-                            } else {
-                                //Create a pipe for the child
-
-                                int pid = fork();
-                                if(pid == 0){
-                                    //The child
-                                    ioRedirect(filev);
-                                    execvp(**argvv, *argvv);
-
-
-                                } else {
-                                    child_pid_bg = pid;
-
-
-                                    if(!in_background){
-                                        wait(&pid);
-                                    }
-
-
-                                }
-                            }
-                        }
-                }
-            }
-        }
-	return 0;
+    int i = 0;
+    for ( i = 0; argvv[num_command][i] != NULL; i++)
+        argv_execvp[i] = argvv[num_command][i];
 }
